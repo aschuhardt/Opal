12ec0ca7affcee53544fa629f48ac32b4f05bf24
Addison Schuhardt <addison@schuhardt.net>
2022-05-05 00:15:23 -0700

    Added relative URI parser tests

--- Opal.Test/DocumentTests.cs
+++ Opal.Test/DocumentTests.cs
@@ -31,6 +31,7 @@ internal class DocumentTests
     [TestCase("=> http://localhost/something else", typeof(LinkLine))]
     [TestCase("=>    http://localhost/something else", typeof(LinkLine))]
     [TestCase("* something", typeof(ListLine))]
+    [TestCase("* 연주가", typeof(ListLine))]
     [TestCase("*    something", typeof(ListLine))]
     [TestCase("", typeof(EmptyLine))]
     [TestCase("   ", typeof(EmptyLine))]
@@ -43,21 +44,54 @@ internal class DocumentTests
     [TestCase("## ", typeof(TextLine))]
     [TestCase("### ", typeof(TextLine))]
     [TestCase("something", typeof(TextLine))]
+    [TestCase("연주가", typeof(TextLine))]
     [TestCase("    something", typeof(TextLine))]
+    [TestCase("    연주가", typeof(TextLine))]
     [TestCase("> something", typeof(QuoteLine))]
+    [TestCase("> 연주가", typeof(QuoteLine))]
     [TestCase(">       something", typeof(QuoteLine))]
     [TestCase("# something", typeof(HeadingLine))]
+    [TestCase("# 연주가", typeof(HeadingLine))]
     [TestCase("#       something", typeof(HeadingLine))]
     [TestCase("## something", typeof(HeadingLine))]
+    [TestCase("## 연주가", typeof(HeadingLine))]
     [TestCase("##       something", typeof(HeadingLine))]
     [TestCase("### something", typeof(HeadingLine))]
+    [TestCase("### 연주가", typeof(HeadingLine))]
     [TestCase("###       something", typeof(HeadingLine))]
+    [TestCase("#### something", typeof(TextLine))]
     [TestCase("``` something", typeof(FormattedBeginLine))]
+    [TestCase("``` 연주가", typeof(FormattedBeginLine))]
     [TestCase("```", typeof(FormattedBeginLine))]
-    public void DocumentParserResolvesCorrectLineType(string line, Type expectedType)
+    public void DocumentParser_ResolvesCorrectLineType(string line, Type expectedType)
     {
         var withLineEnding = new StringBuilder(line, line.Length + 1).AppendLine();
         using var stream = new MemoryStream(Encoding.UTF8.GetBytes(withLineEnding.ToString()));
         Assert.IsAssignableFrom(expectedType, _parser.ParseDocument(stream).Single());
+    }
+
+    [TestCase("=> /something/else", null, "/something/else", null)]
+    [TestCase("=> /something/else hello world", "hello world", "/something/else", null)]
+    [TestCase("=> /something/else?someInput hello world", "hello world", "/something/else", "?someInput")]
+    [TestCase("=> /something/else?someInput hello 연주가", "hello 연주가", "/something/else", "?someInput")]
+    [TestCase("=> /something/else?연주가", null, "/something/else", "?%EC%97%B0%EC%A3%BC%EA%B0%80")]
+    [TestCase("=> /something/else?연주가 hello", "hello", "/something/else", "?%EC%97%B0%EC%A3%BC%EA%B0%80")]
+    public void DocumentParser_ParsesRelativeLinkLinesCorrectly(string line, string expectedText,
+        string? expectedPath, string? expectedQuery)
+    {
+        var withLineEnding = new StringBuilder(line, line.Length + 1).AppendLine();
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(withLineEnding.ToString()));
+        if (_parser.ParseDocument(stream).Single() is not LinkLine linkLine) 
+            throw new ArgumentException();
+
+        Assert.AreEqual(expectedText, linkLine.Text);
+
+        var uri = linkLine.Uri;
+        Assert.AreEqual(_localhost.Scheme, uri.Scheme);
+        Assert.AreEqual(_localhost.Host, uri.Host);
+        Assert.AreEqual(_localhost.Port, uri.Port);
+        Assert.AreEqual(_localhost.IsDefaultPort, uri.IsDefaultPort);
+        Assert.AreEqual(expectedPath ?? string.Empty, uri.AbsolutePath);
+        Assert.AreEqual(expectedQuery ?? string.Empty, uri.Query);
     }
 }
